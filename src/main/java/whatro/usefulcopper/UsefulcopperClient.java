26075463ea594943795ad4b531f0884428145795
whatro <whatrovideos@gmail.com>
2024-09-14 13:49:45 -0700

    Initial commit

---
+++ src/main/java/whatro/usefulcopper/UsefulcopperClient.java
@@ -0,0 +1,10 @@
+package whatro.usefulcopper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UsefulcopperClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
